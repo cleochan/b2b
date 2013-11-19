7dae944d863016241818a7a6dd7a65cff2628aa9
tim-wu <TimWu@crazysales.com.au>
2013-11-19 10:02:40 +0800

    upload dd shipping file

--- application/controllers/ScheduledController.php
+++ application/controllers/ScheduledController.php
@@ -895,12 +895,12 @@ class ScheduledController extends Zend_Controller_Action
         $params_model   =   new Databases_Tables_Params();
         $feed_model     =   new Algorithms_Core_Feed();
         //$orders_model->item_status          =   4;
-        //$orders_model->item_statuses    =   array(4,5); 
-        $orders_model->limit                =   23;
+        $orders_model->item_statuses    =   array(4,5); 
+        //$orders_model->limit                =   23;
         $time_now                           =   time();
         $time                               =   strtotime( '-1 day', $time_now);
-        //$orders_model->update_start_date    =   date('Y-m-d', $time);  
-        //$orders_model->update_end_date      =   date('Y-m-d', $time_now);
+        $orders_model->update_start_date    =   date('Y-m-d', $time);  
+        $orders_model->update_end_date      =   date('Y-m-d', $time_now);
         $logs_path     =   $params_model->GetVal('logs_path');
         $f_logs_feeds  =   @fopen($logs_path."orderslogs/updateddorders".date('YmdHis').".txt", "w+");
         @fwrite($f_logs_feeds, 'Update DD Orders Begin at:'.date("Y-m-d-H:i:s")."\r\n");
@@ -937,7 +937,7 @@ class ScheduledController extends Zend_Controller_Action
                     @fputcsv($f_dd_order_new, $titile_array);
                     $dd_order_model->update_start_date    =   date('Y-m-d', $time);  
                     $dd_order_model->update_end_date      =   date('Y-m-d', $time_now);
-                    //$dd_order_model->item_statuses        =   array(4,5); 
+                    $dd_order_model->item_statuses        =   array(4,5); 
                     $dd_orders      =   $dd_order_model->getDdorders();
                     if($dd_orders){
                         foreach ($dd_orders as $orde_key => $result){
