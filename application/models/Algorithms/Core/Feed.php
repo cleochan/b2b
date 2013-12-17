6a9159a0cb1a24715d593e4fc59e57d15c593970
tim-wu <TimWu@crazysales.com.au>
2013-12-17 09:46:40 +0800

    fix the bug of data feed of dealsdirect

--- application/models/Algorithms/Core/Feed.php
+++ application/models/Algorithms/Core/Feed.php
@@ -538,7 +538,7 @@ class Algorithms_Core_Feed
                 'csv_path'      =>  'outgoing/inventory/datafeed/processing/',
                 'shipping_path' =>  'outgoing/shipping/processing/',
             );
-            /*
+            
             $ftp    =   new Algorithms_Core_Ftp($merchant_ftp_array['ftp_host'], $merchant_ftp_array['ftp_port'], $merchant_ftp_array['ftp_user'], $merchant_ftp_array['ftp_pass']);
             switch ($type){
                 case 'image':
@@ -581,7 +581,6 @@ class Algorithms_Core_Feed
                     $ftp->close();
                     break;
             }
-             */
         }
     }
 }
